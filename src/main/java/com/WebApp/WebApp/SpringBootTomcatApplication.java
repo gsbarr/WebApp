e7eded7794a753d29714050c88fb5df0cc82aa23
gsbarr <gabriel.s.barreiro@gmail.com>
2023-10-17 18:44:32 -0300

    fas

---
+++ src/main/java/com/WebApp/WebApp/SpringBootTomcatApplication.java
@@ -0,0 +1,15 @@
+package com.WebApp.WebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+//@SpringBootApplication
+public class SpringBootTomcatApplication extends SpringBootServletInitializer {
+/*
+    public static void main(String[] args) {
+        SpringApplication.run(WebAppApplication.class, args);
+    }
+
+ */
+}
--- src/test/java/com/WebApp/WebApp/WebAppApplicationTests.java
+++ src/test/java/com/WebApp/WebApp/WebAppApplicationTests.java
@@ -8,6 +8,7 @@ class WebAppApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assert(true);
 	}
 
 }
